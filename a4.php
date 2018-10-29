809c507d79c900391b71d54361484fa06dd0ebab
michaelrodgers <38140530+michaelrodgers@users.noreply.github.com>
2018-10-29 14:48:38 -0700

    Site Outline Update
    To see the site outline as a working site go to: https://michaelrodgers.azurewebsites.net/itc134/a4.php

---
+++ a4.php
@@ -0,0 +1,20 @@
+<?php include "includes/header.php"?>
+        
+        <article>
+            <h2 name="eunice">Scrum Overview</h2>
+               <h3>Eunice J Lee</h3>
+                    <p>Content goes here.</p>
+        </article>    
+        <article>
+            <h2 name="michael">Example Scenerios and Use Cases</h2>
+               <h3>Michael Rodgers</h3>
+                    <p>Content goes here.</p>
+        </article>  
+        <article>
+            <h2 name="taekyoung">Benefits of Scrum</h2>
+               <h3>Taekyoung Kim</h3>
+                    <p>Content goes here.</p>
+        </article>
+
+<?php include "includes/footer.php"?>
+
